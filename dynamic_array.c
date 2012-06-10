71d5dadd7e16e85f4c5ccb8b1467977600c37046
Carl Johnson <cjvoid@gmail.com>
2012-06-10 18:22:17 +0000

    Add dynamic array type: `dyay` for language.

---
+++ dynamic_array.c
@@ -0,0 +1,77 @@
+#include "value.h"
+#include "state.h"
+#include "memory.h"
+#include <stdlib.h>
+#include <assert.h>
+
+#define MAX_ADD 16
+
+struct dyay *dyay_new(int count) {
+	struct dyay *x = NULL;
+	if (count <= 0)
+		count = 0;
+	x = gc_alloc(&vm()->gc, sizeof(*x), T_DYAY);
+	x->count = count;
+	x->max = count == 0 ? 0 : count + MAX_ADD;
+	if (x->max > 0)
+		x->elem = vm_zalloc(sizeof(*x->elem) * x->max);
+	return x;
+}
+
+void dyay_final(struct dyay *arr) {
+	if (arr->elem) {
+		assert(arr->max > 0);
+		vm_free(arr->elem);
+	}
+}
+
+int dyay_equals(const struct dyay *arr, const struct dyay *rhs) {
+	if (arr == rhs)
+		return 1;
+	if (arr->count == rhs->count) {
+		int i = arr->count;
+		while (i--) {
+			if (!equals(arr->elem + i, rhs->elem + i))
+				return 0;
+		}
+		return 1;
+	}
+	return 0;
+}
+
+int dyay_compare(const struct dyay *arr, const struct dyay *rhs) {
+	int i, k;
+	if (arr == rhs)
+		return 0;
+	k = arr->count < rhs->count ? arr->count : rhs->count;
+	for (i = 0; i < k; ++i) {
+		if (compare(arr->elem + i, rhs->elem + i) < 0)
+			return -1;
+		else if (compare(arr->elem + i, rhs->elem + i) > 0)
+			return 1;
+	}
+	if (arr->count < rhs->count)
+		return -1;
+	else if (arr->count > rhs->count)
+		return 1;
+	return 0;
+}
+
+struct variable *dyay_get(struct dyay *arr, ymd_int_t i) {
+	size_t pos = (size_t)i;
+	if (i < 0LL || i >= (ymd_int_t)arr->count)
+		return knax;
+	return arr->elem + pos;
+}
+
+static inline void resize(struct dyay *arr) {
+	arr->max = arr->count * 3 / 2 + MAX_ADD;
+	arr->elem = vm_realloc(arr->elem, sizeof(*arr->elem) * arr->max);
+}
+
+struct variable *dyay_add(struct dyay *arr) {
+	if (arr->count >= arr->max) // Resize
+		resize(arr);
+	memset(arr->elem + arr->count, 0, sizeof(*arr->elem));
+	return arr->elem + arr->count++;
+}
---
+++ dynamic_array_test.c
@@ -0,0 +1,129 @@
+#include "value.h"
+#include "memory.h"
+#include "state.h"
+#include "yut_rand.h"
+#include "yut.h"
+
+#define gc(p) ((struct gc_node *)(p))
+
+static struct dyay *arr;
+
+static int test_dyay_creation_1() {
+	struct variable *rv;
+
+	ASSERT_EQ(uint, arr->count, 0);
+	ASSERT_EQ(uint, arr->max, 0);
+
+	rv = dyay_add(arr);
+	ASSERT_NOTNULL(rv);
+	ASSERT_FALSE(rv == knax);
+	ASSERT_EQ(uint, rv->type, T_NIL);
+	ASSERT_EQ(large, rv->value.i, 0LL);
+	ASSERT_EQ(uint, arr->count, 1);
+	ASSERT_EQ(uint, arr->max, 16);
+
+	arr = dyay_new(13);
+	ASSERT_NOTNULL(arr);
+	ASSERT_EQ(uint, arr->count, 13);
+	ASSERT_EQ(uint, arr->max, 13 + 16);
+	return 0;
+}
+
+static int test_dyay_addition_1() {
+	struct variable *rv;
+	static const int k = 41;
+	int i = k;
+
+	ASSERT_EQ(uint, arr->count, 0);
+	ASSERT_EQ(uint, arr->max, 0);
+
+	while (i--) {
+		rv = dyay_add(arr);
+		ASSERT_NOTNULL(rv);
+		ASSERT_FALSE(rv == knax);
+		ASSERT_EQ(uint, rv->type, T_NIL);
+		ASSERT_EQ(large, rv->value.i, 0LL);
+		ASSERT_EQ(uint, arr->count, k - i);
+
+		rv->type = T_INT;
+		rv->value.i = k - i - 1;
+	}
+	ASSERT_EQ(uint, arr->max, (arr->count - 1) * 3 / 2 + 16);
+	i = k;
+	while (i--) {
+		rv = dyay_get(arr, i);
+		ASSERT_EQ(uint, rv->type, T_INT);
+		ASSERT_EQ(large, rv->value.i, i);
+	}
+	return 0;
+}
+
+#define BENCHMARK_COUNT 100000
+
+static int test_dyay_addition_2() {
+	struct variable *rv;
+	int i = BENCHMARK_COUNT;
+	TIME_RECORD_BEGIN(addition)
+	while (i--) {
+		rv = dyay_add(arr);
+		rv->type = T_INT;
+		rv->value.i = BENCHMARK_COUNT - i - 1;
+	}
+	TIME_RECORD_END
+	i = BENCHMARK_COUNT;
+	RAND_BEGIN(NORMAL)
+		while (i--) {
+			size_t index = RAND_RANGE(ularge, 0, BENCHMARK_COUNT);
+			rv = dyay_get(arr, index);
+			ASSERT_EQ(uint, rv->type, T_INT);
+			ASSERT_EQ(large, rv->value.i, index);
+		}
+	RAND_END
+	return 0;
+}
+
+static void add_int(struct dyay *dya, ymd_int_t i) {
+	struct variable *rv = dyay_add(dya);
+	rv->type = T_INT;
+	rv->value.i = i;
+}
+
+static int test_dyay_comparation() {
+	struct dyay *rhs = dyay_new(0);
+
+	add_int(arr, 0);
+	add_int(arr, 1);
+	add_int(rhs, 0);
+	ASSERT_GT(int, dyay_compare(arr, rhs), 0);
+	ASSERT_FALSE(dyay_equals(arr, rhs));
+
+	add_int(rhs, 1);
+	ASSERT_EQ(int, dyay_compare(arr, rhs), 0);
+	ASSERT_TRUE(dyay_equals(arr, rhs));
+
+	add_int(rhs, 0);
+	ASSERT_LT(int, dyay_compare(arr, rhs), 0);
+	ASSERT_FALSE(dyay_equals(arr, rhs));
+
+	ASSERT_TRUE(dyay_equals(arr, arr));
+	ASSERT_EQ(int, dyay_compare(arr, arr), 0);
+	ASSERT_TRUE(dyay_equals(rhs, rhs));
+	ASSERT_EQ(int, dyay_compare(rhs, rhs), 0);
+	return 0;
+}
+
+static int test_dyay_setup() {
+	arr = dyay_new(0);
+	return 0;
+}
+
+static void test_dyay_teardown() {
+	arr = NULL;
+}
+
+TEST_BEGIN_WITH(test_dyay_setup, test_dyay_teardown)
+	TEST_ENTRY(dyay_creation_1, normal)
+	TEST_ENTRY(dyay_addition_1, normal)
+	TEST_ENTRY(dyay_addition_2, benchmark)
+	TEST_ENTRY(dyay_comparation, normal)
+TEST_END
