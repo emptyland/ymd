150c7963263a6af0588d7597799169dbbea3aa64
Carl Johnson <cjvoid@gmail.com>
2013-01-03 00:55:38 +0800

    Add print windows file for color printing.

--- src/print.h
+++ src/print.h
@@ -27,10 +27,16 @@ enum ymd_color_e {
 // Green color : "${[green]The Green string}$"
 int ymd_vfprintf(FILE *fp, const char *fmt, va_list ap);
 
+#if defined(_MSC_VER)
+int ymd_fprintf(FILE *fp, const char *fmt, ...);
+
+int ymd_printf(const char *fmt, ...);
+#else
 int ymd_fprintf(FILE *fp, const char *fmt, ...)
 	__attribute__ ((__format__ (__printf__, 2, 3)));
 
 int ymd_printf(const char *fmt, ...)
 	__attribute__ ((__format__ (__printf__, 1, 2)));
+#endif
 
 #endif // YMD_PRINT_H
---
+++ src/print_win.c
@@ -0,0 +1,14 @@
+#include "print.h"
+#include <windows.h>
+
+int ymd_vfprintf(FILE *fp, const char *fmt, va_list ap) {
+	return 0;
+}
+
+int ymd_fprintf(FILE *fp, const char *fmt, ...) {
+	return 0;
+}
+
+int ymd_printf(const char *fmt, ...) {
+	return 0;
+}
